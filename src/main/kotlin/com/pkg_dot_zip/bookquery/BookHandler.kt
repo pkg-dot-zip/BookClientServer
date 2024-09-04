9c387e066b4e3fed05541d68046664e1acb93464
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-09-04 16:08:43 +0200

    Can now print all requirements

--- src/main/kotlin/com/pkg_dot_zip/bookquery/BookHandler.kt
+++ src/main/kotlin/com/pkg_dot_zip/bookquery/BookHandler.kt
@@ -9,20 +9,39 @@ object BookHandler {
 
     private const val CHARSET = "UTF-8"
 
-    fun printAll(isbn: String) {
+    private fun getJsonResult(isbn: String) : JsonResult {
         val bookInfo = URL("https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn")
 
         val input = bookInfo.openStream()
         val reader: Reader = InputStreamReader(input, CHARSET)
-        val result = Gson().fromJson(reader, JsonResult::class.java)
+        return Gson().fromJson(reader, JsonResult::class.java)
+    }
+
+    fun printAll(isbn: String) {
+        val result = getJsonResult(isbn)
 
         //Output
         println("ISBN: $isbn")
-        println("Title: " + result.getBookDetail().title)
-        println("Subtitle: " + result.getBookDetail().subTitle)
+        println("Title: ${result.getBookDetail().title}")
+        println("Subtitle: ${result.getBookDetail().subTitle}")
         result.getBookDetail().authors?.forEach { println("Author: $it") }
-        println("Description: " + result.getBookDetail().description)
-        println("Pages: " + result.getBookDetail().pageCount)
-        println("Language: " + result.getBookDetail().language)
+        println("Description: ${result.getBookDetail().description}")
+        println("Pages: ${result.getBookDetail().pageCount}")
+        println("Language: ${result.getBookDetail().language}")
+    }
+
+    fun getTitle(isbn: String) : String {
+        val result = getJsonResult(isbn)
+        return "Title: ${result.getBookDetail().title}"
+    }
+
+    fun getSubtitle(isbn: String) : String {
+        val result = getJsonResult(isbn)
+        return "Subtitle: ${result.getBookDetail().subTitle}"
+    }
+
+    fun getAuthors(isbn: String) : String {
+        val result = getJsonResult(isbn)
+        return "Authors: ${result.getBookDetail().authors?.joinToString()}"
     }
 }
--- src/main/kotlin/com/pkg_dot_zip/server/BookProtocol.kt
+++ src/main/kotlin/com/pkg_dot_zip/server/BookProtocol.kt
@@ -5,35 +5,44 @@ import com.pkg_dot_zip.com.pkg_dot_zip.bookquery.BookHandler
 class BookProtocol {
     private var state = WAITING
     private var currentJoke = 0
+    private var currentISBN: String = ""
+
+    private val INPUT_SWITCH = "switch"
+    private val INPUT_QUIT = "quit"
+    private val INPUT_TITLE = "title"
+    private val INPUT_SUBTITLE = "subtitle"
+    private val INPUT_AUTHORS = "authors"
 
     fun processInput(theInput: String): String? {
         var theOutput: String? = null
-
-
 
         if (state == WAITING) {
             theOutput = "Please input book ISBN:"
             state = SENTBOOKISBN
         } else if (state == SENTBOOKISBN) {
             if (theInput.isNotEmpty()) { // TODO: Implement ISBN regex check.
-                BookHandler.printAll(theInput) // TODO: Store book here ONLY for this specific client (maybe in a map?)
+                currentISBN = theInput // TODO: Store book here ONLY for this specific client (maybe in a map?)
+                theOutput = "Please request either $INPUT_TITLE, $INPUT_SUBTITLE or $INPUT_AUTHORS. Type $INPUT_SWITCH to switch books. Type $INPUT_QUIT to quit."
                 state = SENDINFO
             } else {
                 theOutput = "You're supposed to say \"Who's there?\"! " +
                         "Try again. Knock! Knock!"
             }
         } else if (state == SENDINFO) {
-            // TODO: Prompt user for de titel, de sub-titel en de auteurs.
-        } else if (state == ANOTHER) {
-            if (theInput.equals("y", ignoreCase = true)) {
-                theOutput = "Knock! Knock!"
-                if (currentJoke == (NUMJOKES - 1)) currentJoke = 0
-                else currentJoke++
-                state = SENTBOOKISBN
-            } else {
-                theOutput = "Bye."
-                state = WAITING
+            when (theInput) {
+                INPUT_TITLE -> theOutput = "Title: ${BookHandler.getTitle(currentISBN)}"
+                INPUT_SUBTITLE -> theOutput = "Subtitle: ${BookHandler.getSubtitle(currentISBN)}"
+                INPUT_AUTHORS -> theOutput = "Authors: ${BookHandler.getAuthors(currentISBN)}"
+                INPUT_SWITCH -> {
+                    state = WAITING
+                    return processInput("")
+                }
+                INPUT_QUIT -> {
+                    theOutput = "Bye."
+                    state = WAITING
+                }
             }
+
         }
         return theOutput
     }
@@ -42,8 +51,5 @@ class BookProtocol {
         private const val WAITING = 0
         private const val SENTBOOKISBN = 1
         private const val SENDINFO = 2
-        private const val ANOTHER = 3
-
-        private const val NUMJOKES = 5
     }
 }
