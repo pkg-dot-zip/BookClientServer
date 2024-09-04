33c219286730bbf53eb772dd8d84f453d19d6791
pkg-dot-zip <simon@meneerdegroot.nl>
2024-09-04 21:10:27 +0200

    States are now an enum

--- src/main/kotlin/com/pkg_dot_zip/server/BookProtocol.kt
+++ src/main/kotlin/com/pkg_dot_zip/server/BookProtocol.kt
@@ -3,49 +3,55 @@ package com.pkg_dot_zip.com.pkg_dot_zip.server
 import com.pkg_dot_zip.com.pkg_dot_zip.bookquery.BookHandler
 
 class BookProtocol {
-    private var state = WAITING
+    private var state = State.WAITING
     private var currentISBN: String = ""
 
     fun processInput(theInput: String): String? {
         var theOutput: String? = null
 
-        if (state == WAITING) {
-            theOutput = "Please input book ISBN:"
-            state = SENTBOOKISBN
-        } else if (state == SENTBOOKISBN) {
-            if (theInput.isNotEmpty()) { // TODO: Implement ISBN regex check.
-                currentISBN = theInput // TODO: Store book here ONLY for this specific client (maybe in a map?)
-                theOutput = "Please request either $INPUT_TITLE, $INPUT_SUBTITLE or $INPUT_AUTHORS. Type $INPUT_SWITCH to switch books. Type $INPUT_QUIT to quit."
-                state = SENDINFO
-            } else {
-                theOutput = "Please insert a valid ISBN"
+        when (state) {
+            State.WAITING -> {
+                theOutput = "Please input book ISBN:"
+                state = State.SENT_BOOK_ISBN
             }
-        } else if (state == SENDINFO) {
-            when (theInput) {
-                INPUT_TITLE -> theOutput = BookHandler.getTitle(currentISBN)
-                INPUT_SUBTITLE -> theOutput = BookHandler.getSubtitle(currentISBN)
-                INPUT_AUTHORS -> theOutput = BookHandler.getAuthors(currentISBN)
-                INPUT_SWITCH -> {
-                    state = WAITING
-                    return processInput("")
-                }
-                INPUT_QUIT -> {
-                    theOutput = "Bye."
-                    state = WAITING
+
+            State.SENT_BOOK_ISBN -> {
+                if (theInput.isNotEmpty()) { // TODO: Implement ISBN regex check.
+                    currentISBN = theInput // TODO: Store book here ONLY for this specific client (maybe in a map?)
+                    theOutput =
+                        "Please request either $INPUT_TITLE, $INPUT_SUBTITLE or $INPUT_AUTHORS. Type $INPUT_SWITCH to switch books. Type $INPUT_QUIT to quit."
+                    state = State.SEND_INFO
+                } else {
+                    theOutput = "Please insert a valid ISBN"
                 }
             }
 
+            State.SEND_INFO -> {
+                when (theInput) {
+                    INPUT_TITLE -> theOutput = BookHandler.getTitle(currentISBN)
+                    INPUT_SUBTITLE -> theOutput = BookHandler.getSubtitle(currentISBN)
+                    INPUT_AUTHORS -> theOutput = BookHandler.getAuthors(currentISBN)
+                    INPUT_SWITCH -> {
+                        state = State.WAITING
+                        return processInput("")
+                    }
+
+                    INPUT_QUIT -> {
+                        theOutput = "Bye."
+                        state = State.WAITING
+                    }
+                }
+            }
         }
         return theOutput
     }
 
     companion object {
-        //<editor-fold desc="States">
-        private const val WAITING = 0
-        private const val SENTBOOKISBN = 1
-        private const val SENDINFO = 2
-        //</editor-fold>
-
+        enum class State {
+            WAITING,
+            SENT_BOOK_ISBN,
+            SEND_INFO;
+        }
 
         //<editor-fold desc="Commands">
         private const val INPUT_SWITCH = "switch"
